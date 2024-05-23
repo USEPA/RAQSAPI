23f4cd725a335460d15d59b8f5d8bcfce11a6f2b
Clinton Mccrowey <mccrowey.clinton@epa.gov>
2024-05-23 16:46:47 -0400

    fix testing credentials in unit tests

--- tests/testthat/test-byMA.R
+++ tests/testthat/test-byMA.R
@@ -11,8 +11,8 @@ test_that("byMA functions", {
     datamartAPI_user <- AQScredentials$datamartAPI_user
     datamartAPI_key <- AQScredentials$datamartAPI_key
   } else {
-    datamartAPI_user <- Sys.getenv("RAQSAPIKEY", names = TRUE)
-    datamartAPI_key <- Sys.getenv("RAQSAPIUSERNAME", names = TRUE)
+    datamartAPI_user <- Sys.getenv("RAQSAPIUSERNAME", names = TRUE)
+    datamartAPI_key <- Sys.getenv("RAQSAPIKEY", names = TRUE)
   }
   RAQSAPI::aqs_credentials(username = datamartAPI_user,
                            key = datamartAPI_key
