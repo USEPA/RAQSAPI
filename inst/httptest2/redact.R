a4c63a37a08cb70181dbec1f1f8284e43b96eeb1
AA\CMcCrowe <mccrowey.clinton@epa.gov>
2025-09-05 15:16:40 -0400

    add .devcontainer/ for codespace, changes to inst/httptest2/redact.R to address issues with unit tests mocks, add test_httptest2 branch to pgkcheck github workflow

--- inst/httptest2/redact.R
+++ inst/httptest2/redact.R
@@ -7,8 +7,8 @@
 # }
 
 
-set_redactor(~ gsub_response(.,getOption(x="aqs_key", default=""), "redacted", fixed = TRUE) %>%
-               gsub_response(.,getOption(x="aqs_username", default=""), "redacted", fixed = TRUE) %>%
+set_redactor(~ gsub_response(., getOption("aqs_key", ""), "<redacted>", fixed = TRUE) %>%
+               gsub_response(., getOption("aqs_username", ""), "<redacted>", fixed = TRUE) %>%
                #shorten mock path by removing the base URL
                gsub_response(., "aqs.epa.gov/data/api/", "datamart/", fixed = TRUE) %>%
                #abbreviate long function causes path in mocks
